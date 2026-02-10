db5ab30d75c06fba49c4fec105fbe393da492c60
surajkunte <94896815+surajkunte@users.noreply.github.com>
2026-02-10 18:40:03 +0530

    Update Booking.java

--- model/Booking.java
+++ model/Booking.java
@@ -1,4 +1,11 @@
 package model;
 public class Booking {
-    
+    private String bookingId;
+    Flight flight;
+    Map<Seat, Passenger>seatPassengerMap;
+    private BookingStatus bookingStatus; 
+
+    public void cancel(){
+        status = BookingStatus.CANCELLED;
+    }
 }
